1169159cb1dd53d3498b0c95e4525a802730c381
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-05 23:41:24 +0800

    如果採用，此提交將完成題目1-6 1-2
    修改項目:

---
+++ Own practice/1_6/1_2.py
@@ -0,0 +1,11 @@
+"""
+程式設計練習題 1-6 1-2 顯示相同的訊息五次.
+
+請撰寫一程式，顯示五次"Welcome to Python"。
+"""
+
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
