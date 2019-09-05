eba2e827fa5829ac43d48b8745578a525a4a747f
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-05 22:51:31 +0800

    如果採用，此提交將修改並讓說明完整
    修改項目:

---
+++ Own practice/1_6/1_1.py
@@ -0,0 +1,10 @@
+"""
+Python programming exercises 1-6 1-1.
+
+顯示三個不同的訊息，顯示"Welcome to Python"、"Welcome to Computer"以及"Programming
+is fun"。
+"""
+
+print("Welcome to Python")
+print("Welcome to Computer")
+print("Programming is fun")
