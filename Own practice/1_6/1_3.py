15ca1882df8f3de352d41f879f0216e81f5e21a4
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-05 23:41:36 +0800

    如果採用，此提交將題目1-6 1-3
    修改項目:

---
+++ Own practice/1_6/1_3.py
@@ -0,0 +1,16 @@
+"""
+程式設計練習題 1-6 1-3 顯示圖樣.
+
+請撰寫一程式，顯示如下的圖案
+FFFFFFF   U    U   NN    NN
+FF        U    U   NNN   NN
+FFFFFFF   U    U   NN N  NN
+FF         U  U    NN  N NN
+FF          UU     NN   NNN
+"""
+
+print("FFFFFFF   U    U   NN    NN")
+print("FF        U    U   NNN   NN")
+print("FFFFFFF   U    U   NN N  NN")
+print("FF         U  U    NN  N NN")
+print("FF          UU     NN   NNN")
