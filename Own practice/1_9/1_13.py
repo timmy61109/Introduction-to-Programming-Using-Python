7adf5be2376c968c9f904fe53b11d58618ea8a73
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-08 17:13:09 +0800

    如果採用，此提交將完成程式設計練習題 1-6 1-13 Turtle:畫十字形.
    修改項目:

---
+++ Own practice/1_9/1_13.py
@@ -0,0 +1,17 @@
+"""
+程式設計練習題 1-6 1-13 Turtle:畫十字形.
+
+撰寫一程式，在螢幕的中央畫十字形。
+"""
+
+import turtle
+turtle.showturtle()
+turtle.forward(50)
+turtle.right(180)
+turtle.forward(100)
+turtle.right(180)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(180)
+turtle.forward(100)
