feb5c48717b49d2e6fdec53de0412d2b7e4353d3
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-09-15 23:34:47 +0800

    如果採用，此提交將完成2.1~2.3.
    修改項目:
    - 2.1
    - 2.2
    - 2.3

---
+++ Own practice/2.2-2.10/2.3.py
@@ -0,0 +1,16 @@
+"""
+程式設計練習題 2.2-2.10 2.3 將英吋轉換成公尺.
+
+請撰寫一程式，讀取英吋(feet)數，將期轉換為公尺(meter)，秉顯示結果。一英吋相當於.0.305公尺。以
+下是範例輸出樣本:
+```
+Enter a value for feet:16.5
+16.5 feet is 5.0325 meters
+```
+"""
+
+feet = eval(input("Enter a value for feet:"))
+
+meters = feet * 0.305
+
+print(feet, 'feet is', meters, "meters")
