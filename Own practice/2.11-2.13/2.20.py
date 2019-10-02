acffa5c8240a35adb83cfd988f69f677333beebc
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-10-03 00:51:18 +0800

    如果採用，此提交將新增完成的作業與修改錯誤之處
    修改項目:

---
+++ Own practice/2.11-2.13/2.20.py
@@ -0,0 +1,20 @@
+"""
+程式設計練習題 2.2-2.10 2.20 財務金融應用程式:計算利息.
+
+利息公式:
+
+interest = balance * (annualInterestRate / 1200)
+
+以下是輸出樣本:
+```
+Enter balance and interest rate (e.g., 3 for 3%):1000, 3.5
+The interest is 1043.33
+```
+"""
+
+
+balance, annualInterestRate = eval(input("Enter investment amount:"))
+
+interest = balance * (annualInterestRate / 1200)
+
+print("The interest is", interest)
---
+++ Own practice/2.11-2.13/2.21.py
@@ -0,0 +1,37 @@
+"""
+程式設計練習題 2.2-2.10 2.21 財務金融應用程式:複利值.
+
+利息公式:
+
+100 * (1 + 0.00417) = 100.417
+(100 + 100.417) * (1 + 0.00417) = 201.252
+(100 + 201.252) * (1 + 0.00417) = 302.507
+
+以下是輸出樣本:
+```
+Enter the monthly saving amount:100
+After the sixth month, the account value is 608.81
+```
+"""
+
+amount = eval(input("Enter the monthly saving amount:"))
+
+account_value = amount
+amount *= (1 + 0.00417)
+
+amount = amount + account_value
+amount *= (1 + 0.00417)
+
+amount = amount + account_value
+amount *= (1 + 0.00417)
+
+amount = amount + account_value
+amount *= (1 + 0.00417)
+
+amount = amount + account_value
+amount *= (1 + 0.00417)
+
+amount = amount + account_value
+amount *= (1 + 0.00417)
+
+print("After the sixth month, the account value is", amount)
