7a23a27a5cec22407bfcc2bc28b5eed212d7138b
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-11-07 20:35:14 +0800

    刪除不需要的檔案，解決CI驗證錯誤問題

--- evennumberedexercise/Exercise13_6.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-def main():
-    infile = urllib.request.urlopen("http://cs.armstrong.edu/liang/data/Lincoln.txt")
-    s = infile.read()
-
-    print("The number of words in the file is " + str(len(s.split())))
-main()
--- evennumberedexercise/Exercise2_4.py
+++
@@ -1,5 +0,0 @@
-pounds = eval(input("Enter a number in pounds: "))
-
-kilograms = pounds * 0.454;
-
-print(str(pounds) + " pounds is " + str(kilograms) + " kilograms")
