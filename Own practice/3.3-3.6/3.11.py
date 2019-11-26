f23b544582f4baebf7a1281ba5d8e35704f96542
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2019-11-27 00:53:36 +0800

    如果採用，此提交將完成程式設計練習題3.11

--- Own practice/3.3-3.6/3.11.py
+++ Own practice/3.3-3.6/3.11.py
@@ -4,28 +4,20 @@
 請撰寫一程式，提示使用者輸入四位數的整數，然後以反轉的順序加以印出，以下是程式輸出的樣本:
 
 ```
-
+Enter an integer:1325
+The reversed number is 5213
 ```
 """
 # Receive the amount
-name = str(input("Enter employee's name:"))
-hours = eval(input("Enter number of hours worked in a week:"))
-hourly = eval(input("Enter hourly pay rate:"))
-federal = eval(input("Enter federal tax wighholding rate:"))
-state = eval(input("Enter state tax withholding rate:"))
+number = int(input("Enter employee's name:"))
 
-gross_pay = hours * hourly
-federal_withholding = gross_pay * 0.2
-state_withholding = gross_pay * 0.09
-total_deduction = federal_withholding + state_withholding
-net_pay = gross_pay - total_deduction
 
-print("\nEmployee Name:", name, "\n",
-      "Hours Worked:", hours, "\n",
-      "Pay Rate $:", hourly, "\n",
-      "Gross Pay: $", gross_pay, "\n",
-      "Deuctions", gross_pay, "\n",
-      "  Federal Withholding (20.0%): $", federal_withholding, "\n",
-      "  State Withholding (9.0%): $", state_withholding, "\n",
-      "  Total Deduction: $", total_deduction, "\n",
-      "Net Pay: $", net_pay)
+Thousands = str(number // 1000)
+number %= 1000
+Hundreds = str(number // 100)
+number %= 100
+Tens = str(number // 10)
+number %= 10
+Each = str(number // 1)
+
+print(Each + Tens + Hundreds + Thousands)
