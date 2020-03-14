03ddd692c2b7c19b5c5be40128f7f551ca6ec496
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-03-14 10:04:15 +0800

    如果採用，此提交將完成程式設計練習題3.15與3.16

---
+++ own_practice/three_fifteen.py
@@ -0,0 +1,62 @@
+u"""
+程式設計練習題 3.3-3.6 3.15 Turtle:畫出一笑臉.
+
+請撰寫一程式顯示笑臉的程式。
+"""
+from turtle import Screen, Turtle
+
+
+TURTLE = Turtle()
+SCREEN = Screen()
+
+TURTLE.hideturtle()
+
+TURTLE.penup()
+TURTLE.goto(0, -200)
+TURTLE.pendown()
+TURTLE.circle(200)
+
+TURTLE.penup()
+TURTLE.goto(80, 40)
+TURTLE.pendown()
+TURTLE.begin_fill()
+TURTLE.color("red")
+TURTLE.fillcolor("red")
+TURTLE.circle(20)
+TURTLE.end_fill()
+
+TURTLE.penup()
+TURTLE.goto(-80, 40)
+TURTLE.pendown()
+TURTLE.begin_fill()
+TURTLE.color("red")
+TURTLE.fillcolor("red")
+TURTLE.circle(20)
+TURTLE.end_fill()
+
+TURTLE.penup()
+TURTLE.goto(0, 0)
+TURTLE.pendown()
+TURTLE.color("black")
+TURTLE.dot()
+
+TURTLE.penup()
+TURTLE.goto(60, -(60 * (3 ** 0.5)) / 3)
+TURTLE.left(60)
+TURTLE.pendown()
+TURTLE.color("black")
+TURTLE.circle((120 * (3 ** 0.5)) / 3, steps=3)
+
+TURTLE.penup()
+TURTLE.goto(0, (-(120 * (3 ** 0.5)) / 3) * 2)
+TURTLE.pendown()
+TURTLE.right(30)
+TURTLE.forward((240 * (3 ** 0.5)) / 3 + 10)
+
+TURTLE.penup()
+TURTLE.goto(0, (-(120 * (3 ** 0.5)) / 3) * 2)
+TURTLE.pendown()
+TURTLE.left(120)
+TURTLE.forward((240 * (3 ** 0.5)) / 3 + 10)
+
+SCREEN.mainloop()
---
+++ own_practice/three_sixteen.py
@@ -0,0 +1,69 @@
+u"""
+程式設計練習題 3.3-3.6 3.16 Turtle:畫出一些形狀.
+
+請撰寫一程式顯示笑臉的程式。
+"""
+from turtle import Screen, Turtle
+
+
+TURTLE = Turtle()
+SCREEN = Screen()
+
+TURTLE.pensize(3)  # Set pen thickness to 3 pixels
+TURTLE.penup()  # Pull the pen up
+TURTLE.goto(-200, -50)
+TURTLE.left(60)  # Draw a triangle
+TURTLE.pendown()  # Pull the pen down
+TURTLE.begin_fill()  # Begin to fill color in a shape
+TURTLE.color("red")
+TURTLE.circle(40, steps=3)  # Draw a triangle
+TURTLE.end_fill()  # Fill the shape
+
+TURTLE.penup()
+TURTLE.goto(-100, -50)
+TURTLE.setheading(0)
+TURTLE.left(45)
+TURTLE.pendown()
+TURTLE.begin_fill()  # Begin to fill color in a shape
+TURTLE.color("blue")
+TURTLE.circle(40, steps=4)  # Draw a square
+TURTLE.end_fill()  # Fill the shape
+
+TURTLE.penup()
+TURTLE.goto(0, -50)
+TURTLE.setheading(0)
+TURTLE.left(36)
+TURTLE.pendown()
+TURTLE.begin_fill()  # Begin to fill color in a shape
+TURTLE.color("green")
+TURTLE.circle(40, steps=5)  # Draw a pentagon
+TURTLE.end_fill()  # Fill the shape
+
+TURTLE.penup()
+TURTLE.goto(100, -50)
+TURTLE.setheading(0)
+TURTLE.left(30)
+TURTLE.pendown()
+TURTLE.begin_fill()  # Begin to fill color in a shape
+TURTLE.color("yellow")
+TURTLE.circle(40, steps=6)  # Draw a hexagon
+TURTLE.end_fill()  # Fill the shape
+
+TURTLE.penup()
+TURTLE.goto(200, -50)
+TURTLE.setheading(0)
+TURTLE.left(22.5)
+TURTLE.pendown()
+TURTLE.begin_fill()  # Begin to fill color in a shape
+TURTLE.color("purple")
+TURTLE.circle(40, steps=8)  # Draw a circle
+TURTLE.end_fill()  # Fill the shape
+
+TURTLE.color("green")
+TURTLE.penup()
+TURTLE.goto(-100, 50)
+TURTLE.pendown()
+TURTLE.write("Cool Colorful Shapes", font=("Times", 18, "bold"))
+TURTLE.hideturtle()
+
+SCREEN.mainloop()
