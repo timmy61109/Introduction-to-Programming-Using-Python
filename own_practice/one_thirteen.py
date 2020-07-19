2ea347452ef292989358993d7c3feccee081c896
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-07-19 13:05:28 +0800

    如果採用，此提交將修正Pylint W0123問題
    修改項目:
    
    將eval改為使用ast模組的literal_eval函式。
    
    模組:
    
    議題3

--- own_practice/one_thirteen.py
+++ own_practice/one_thirteen.py
@@ -3,15 +3,17 @@
 
 撰寫一程式，在螢幕的中央畫十字形。
 """
+from turtle import Turtle
 
-import turtle
-turtle.showturtle()
-turtle.forward(50)
-turtle.right(180)
-turtle.forward(100)
-turtle.right(180)
-turtle.forward(50)
-turtle.right(90)
-turtle.forward(50)
-turtle.right(180)
-turtle.forward(100)
+TURTLE = Turtle()
+
+TURTLE.showturtle()
+TURTLE.forward(50)
+TURTLE.right(180)
+TURTLE.forward(100)
+TURTLE.right(180)
+TURTLE.forward(50)
+TURTLE.right(90)
+TURTLE.forward(50)
+TURTLE.right(180)
+TURTLE.forward(100)
