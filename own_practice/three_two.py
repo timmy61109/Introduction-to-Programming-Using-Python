010320690f5446d7b2ee6af6708eb4f2d76f5e61
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-07-19 18:18:13 +0800

    如果採用，此提交將修正Pylint W0123問題
    修改項目:
    
    將eval改為使用ast模組的literal_eval函式。
    
    模組:
    
    議題3

--- own_practice/three_two.py
+++ own_practice/three_two.py
@@ -4,7 +4,7 @@
 撰寫一程式提示使用者輸入球面上兩點，並計算出這兩點間在圓表面的最短距離。
 地球半徑平均6371.01km。
 
-d = radius * acos(sin(x1) * sin(x2) + cos(x1) * cos(x2) * cos(y1 - y2))
+d = RADIUS * acos(sin(x1) * sin(x2) + cos(x1) * cos(x2) * cos(y1 - y2))
 
 以下是程式的執行結果:
 ```
@@ -15,13 +15,15 @@ Enter point 2 (latitude and longitude) in degrees:
 The distance between the two points is 10691.79183231593 km
 ```
 """
+import ast
+
 import math
 
 
-radius = 6371.01
-x1, y1 = eval(
+RADIUS = 6371.01
+x1, y1 = ast.literal_eval(
     input("Enter point 1 (latitude and longitude) in degrees:\n"))
-x2, y2 = eval(
+x2, y2 = ast.literal_eval(
     input("Enter point 2 (latitude and longitude) in degrees:\n"))
 
 x1 = math.radians(x1)
@@ -29,7 +31,7 @@ y1 = math.radians(y1)
 x2 = math.radians(x2)
 y2 = math.radians(y2)
 
-d = radius * math.acos(
+d = RADIUS * math.acos(
     math.sin(x1) * math.sin(x2) + math.cos(x1) * math.cos(x2) * math.cos(
         y1 - y2))
 
