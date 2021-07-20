3dce823a4dc38a1cb116f2cbb46a43cacfc92008
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-07-20 22:52:17 +0800

    修改內容與風格化
    修改項目:
    
    模組:
    
    議題
    
    GitHub
    Jira

--- examples/RandomCharacter.py
+++ examples/RandomCharacter.py
@@ -1,21 +1,26 @@
-from random import randint # import randint
+from random import randint  # import randint
 
-# Generate a random character between ch1 and ch2
+
 def getRandomCharacter(ch1, ch2):
+    """Generate a random character between ch1 and ch2."""
     return chr(randint(ord(ch1), ord(ch2)))
 
-# Generate a random lowercase letter
+
 def getRandomLowerCaseLetter():
+    """Generate a random lowercase letter."""
     return getRandomCharacter('a', 'z')
 
-# Generate a random uppercase letter
+
 def getRandomUpperCaseLetter():
+    """Generate a random uppercase letter."""
     return getRandomCharacter('A', 'Z')
 
-# Generate a random digit character
+
 def getRandomDigitCharacter():
+    """Generate a random digit character."""
     return getRandomCharacter('0', '9')
 
-# Generate a random character
+
 def getRandomASCIICharacter():
+    """Generate a random character."""
     return getRandomCharacter(chr(0), chr(127))
