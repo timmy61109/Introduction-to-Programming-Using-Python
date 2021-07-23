f3794a00d8b0b28907b1cb1b9cc9d01526a4c917
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-07-23 09:27:12 +0800

    ITPUP-94 完成傅立葉轉換
    修改項目:
    
    模組:
    
    議題
    
    GitHub
    Jira

---
+++ own_practice/fft.py
@@ -0,0 +1,24 @@
+"""FFT."""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+SAMPLING_RATE = 44100  # 設定取樣率 8000Hz
+FFT_SIZE = 512  # 設定FFT大小
+
+time_smpline = np.arange(0, 1.0, 1.0 / SAMPLING_RATE)
+
+frequency = np.sin(2 * np.pi * 861.328125 * time_smpline) + \
+    np.sin(2 * np.pi * 8613.28125 * time_smpline)
+
+xs = frequency[:FFT_SIZE]
+xf = np.fft.rfft(xs) / FFT_SIZE
+
+xfp = 20 * np.log10(np.clip(np.abs(xf), 1e-20, 1e100))
+
+plt.plot(xfp, 'b')
+plt.xlabel("Frequency", fontsize=14)  # x label
+plt.ylabel('time(s)', fontsize=14)
+plt.title('FFT')
+plt.savefig('./fft.png')
+plt.show()
