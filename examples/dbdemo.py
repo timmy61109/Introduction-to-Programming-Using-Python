d17e757ddd10c37f8a237e0c1d4ea8a41895d249
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-07-23 13:46:05 +0800

    更新dbdemo.py
    修改項目:
    
    模組:
    
    議題
    
    GitHub
    Jira

--- examples/dbdemo.py
+++ examples/dbdemo.py
@@ -9,13 +9,14 @@ cursor.execute("create table Course ( " +
                "courseNumber integer, title varchar(50) not null, " +
                "numOfCredits integer, primary key (courseId))")
 
-cursor.execute("insert into Course (courseId, subjectId, " + 
-               " courseNumber, title, numOfCredits) " + 
+cursor.execute("insert into Course (courseId, subjectId, " +
+               " courseNumber, title, numOfCredits) " +
                "values ('11113', 'CSCI', '3720', 'Database Systems', 3)")
 
-cursor.execute("insert into Course (courseId, subjectId, " + 
-               " courseNumber, title, numOfCredits) " + 
-               "values ('11111', 'CSCI', '1301', 'Introduction to Programming', 3)")
+cursor.execute("insert into Course (courseId, subjectId, " +
+               " courseNumber, title, numOfCredits) " +
+               "values " +
+               "('11111', 'CSCI', '1301', 'Introduction to Programming', 3)")
 db.commit()
 
 cursor.execute("select * from Course")
