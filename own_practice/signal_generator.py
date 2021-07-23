6d152e9d0edbdeff559123b301369e7ccd6bada3
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-07-23 09:08:18 +0800

    ITPUP-134 更新訊號產生器產生流程
    修改項目:
    
    模組:
    
    議題
    
    GitHub
    Jira

--- own_practice/signal_generator.py
+++ own_practice/signal_generator.py
@@ -8,15 +8,12 @@ FFT_SIZE = 512  # 設定FFT大小
 
 time_smpline = np.arange(0, 1.0, 1.0 / SAMPLING_RATE)
 
-frequency = np.sin(2 * np.pi * 861.328125 * time_smpline) + \
-    np.sin(2 * np.pi * 8613.28125 * time_smpline)
-time_domain = []
-for ele in frequency:
-    time_domain.append(ele)
+frequency = np.sin(2 * np.pi * 86.1328125 * time_smpline) + \
+    np.sin(2 * np.pi * 861.328125 * time_smpline)
 
-plt.plot(time_domain, 'b')
+plt.plot(frequency, 'b')
 plt.xlabel("time(s)", fontsize=14)  # x label
-plt.ylabel('Hz', fontsize=14)
-plt.title('Time Domain')
-plt.savefig('./time_domain.png')
+plt.ylabel('Frequency', fontsize=14)
+plt.title('Signal Generator')
+plt.savefig('./signal_generator.png')
 plt.show()
