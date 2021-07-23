5fe374fd0632acf45ea8c5fa94d64e3c2da0333d
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-07-23 09:09:30 +0800

    ITPUP-93 改動FFT的內容並讓它正確顯示
    修改項目:
    
    模組:
    
    議題
    
    GitHub
    Jira

--- own_practice/time_domain.py
+++ own_practice/time_domain.py
@@ -4,14 +4,9 @@ from scipy.io import wavfile
 
 
 # 讀入單一檔案測試
-_, data_array = wavfile.read('c9DxQmY.wav')  # load the data
-time_domain = []
+sampling_rate, data_array = wavfile.read('c9DxQmY.wav')  # load the data
 
-# this is 16-bit track, b is now normalized on [-1, 1)
-for ele in data_array:
-    time_domain.append(ele / 2 ** 24.)
-
-plt.plot(time_domain, 'b')
+plt.plot(data_array / 2 ** 24.0, 'b')
 plt.xlabel("time(s)", fontsize=14)  # x label
 plt.ylabel('Hz', fontsize=14)
 plt.title('Time Domain')
