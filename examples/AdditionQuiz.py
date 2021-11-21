e8374232a2355d2f878b1f87b4b5436abe96a79c
ellis0817 <john.58991996@gmail.com>
2021-11-21 20:27:24 +0800

    修改以符合EPE8規範
    改變使用的函式eval改為ast.literal_eval

--- examples/AdditionQuiz.py
+++ examples/AdditionQuiz.py
@@ -1,3 +1,8 @@
+"""Additional quiz.
+
+隨機加法練習程式
+"""
+import ast
 import random
 
 # Generate random numbers
@@ -5,9 +10,8 @@ number1 = random.randint(0, 9)
 number2 = random.randint(0, 9)
 
 # Prompt the user to enter an answer
-answer = eval(input("What is " + str(number1) + " + "
-    + str(number2) + "? "))
+answer = ast.literal_eval(
+    input("What is " + str(number1) + " + " + str(number2) + "? "))
 
 # Display result
-print(number1, "+", number2, "=", answer,
-    "is", number1 + number2 == answer)
+print(number1, "+", number2, "=", answer, "is", number1 + number2 == answer)
