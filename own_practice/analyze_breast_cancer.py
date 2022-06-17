1509ed63670c31959d5c5bea0d6d8378c240ea1e
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-06-17 23:23:03 +0800

    ITPUP- 更新分析癌症程式可以循環輸出
    修改項目:
    
    模組:
    
    議題
    
    GitHub
    Jira

--- own_practice/analyze_breast_cancer.py
+++ own_practice/analyze_breast_cancer.py
@@ -1,45 +1,48 @@
 """Analyz."""
 
 import numpy as np
-from plot_decision_regions import plot_decision_regions
 from plot_decision_regions import learn_module
+from plot_decision_regions import plot_decision_regions
 from sklearn.datasets import load_breast_cancer
-from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-MODELS = ['perceptron', 'log', 'svm', 'rbf', 'knn']
+MODELS = ['perceptron', 'log', 'svm', 'rbf', 'knn', 'tree', 'random_forest']
 
-x_data = load_breast_cancer().data
-y_data = load_breast_cancer().target
-class_names = load_breast_cancer().target_names
 
-classes, class_num = np.unique(y_data, return_counts=True)
+for value in range(30):
+    for value_2 in range(30):
+        if value != value_2:
+            x_data = load_breast_cancer().data[:, [value, value_2]]
+            y_data = load_breast_cancer().target
+            class_names = load_breast_cancer().target_names
+            classes, class_num = np.unique(y_data, return_counts=True)
 
-for value in range(31):
-    x_data = PCA(n_components=2).fit_transform(x_data)
-    x_data = StandardScaler().fit_transform(x_data)
+            x_train, x_test, y_train, y_test = train_test_split(
+                x_data, y_data, test_size=0.2, random_state=0)
 
-    x_train, x_test, y_train, y_test = train_test_split(
-        x_data, y_data, test_size=0.2, random_state=0)
+            sc = StandardScaler()
+            sc.fit(x_train)
+            x_train_std = sc.transform(x_train)
+            x_test_std = sc.transform(x_test)
 
-    sc = StandardScaler()
-    sc.fit(x_train)
-    x_train_std = sc.transform(x_train)
-    x_test_std = sc.transform(x_test)
+            for model_name in MODELS:
+                model = learn_module(
+                    x_train_std, y_train, model_name=model_name)
 
-    for model_name in MODELS:
-        model = learn_module(x_train_std, y_train, model_name=model_name)
+                y_pred = model.predict(x_test_std)
+                print('Misclassified samples: %d' % (y_test != y_pred).sum())
 
-        y_pred = model.predict(x_test_std)
-        print('Misclassified samples: %d' % (y_test != y_pred).sum())
+                print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
 
-        print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+                if accuracy_score(y_test, y_pred) >= 0.97:
+                    x_combined_std = np.vstack((x_train_std, x_test_std))
+                    y_combined_std = np.hstack((y_train, y_test))
 
-        x_combined_std = np.vstack((x_train_std, x_test_std))
-        y_combined_std = np.hstack((y_train, y_test))
-
-        plot_decision_regions(
-            x_combined_std, y_combined_std, model_name=model_name,
-            classifier=model, test_idx=range(105, 150))
+                    plot_decision_regions(
+                        x_combined_std, y_combined_std,
+                        model_name=model_name + "_" + str(value) +
+                        'x' + str(value + 1) +
+                        "_" + str(accuracy_score(y_test, y_pred)),
+                        classifier=model, test_idx=range(105, 150))
