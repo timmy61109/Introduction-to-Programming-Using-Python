c15a1bb330caf4795bebc309e4417c7aeab14aba
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-06-17 22:18:58 +0800

    ITPUP- 改為自動產生方式
    修改項目:
    
    模組:
    
    議題
    
    GitHub
    Jira

--- own_practice/analyze_breast_cancer.py
+++ own_practice/analyze_breast_cancer.py
@@ -2,40 +2,44 @@
 
 import numpy as np
 from plot_decision_regions import plot_decision_regions
+from plot_decision_regions import learn_module
 from sklearn.datasets import load_breast_cancer
 from sklearn.decomposition import PCA
-from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-import matplotlib.pyplot as plt
+MODELS = ['perceptron', 'log', 'svm', 'rbf', 'knn']
+
 x_data = load_breast_cancer().data
 y_data = load_breast_cancer().target
 class_names = load_breast_cancer().target_names
 
 classes, class_num = np.unique(y_data, return_counts=True)
 
+for value in range(31):
+    x_data = PCA(n_components=2).fit_transform(x_data)
+    x_data = StandardScaler().fit_transform(x_data)
 
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_data, y_data, test_size=0.2, random_state=0)
 
+    sc = StandardScaler()
+    sc.fit(x_train)
+    x_train_std = sc.transform(x_train)
+    x_test_std = sc.transform(x_test)
 
-x_train, x_test, y_train, y_test = train_test_split(
-    x_data, y_data, test_size=0.2, random_state=0)
+    for model_name in MODELS:
+        model = learn_module(x_train_std, y_train, model_name=model_name)
 
-sc = StandardScaler()
-sc.fit(x_train)
-x_train_std = sc.transform(x_train)
-x_test_std = sc.transform(x_test)
+        y_pred = model.predict(x_test_std)
+        print('Misclassified samples: %d' % (y_test != y_pred).sum())
 
-ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0)
-ppn.fit(x_train_std, y_train)
+        print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
 
-y_pred = ppn.predict(x_test_std)
-print('Misclassified samples: %d' % (y_test != y_pred).sum())
+        x_combined_std = np.vstack((x_train_std, x_test_std))
+        y_combined_std = np.hstack((y_train, y_test))
 
-print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
-
-x_combined_std = np.vstack((x_train_std, x_test_std))
-y_combined_std = np.hstack((y_train, y_test))
-plot_decision_regions(
-    x_combined_std, y_combined_std, classifier=ppn, test_idx=range(105, 150))
+        plot_decision_regions(
+            x_combined_std, y_combined_std, model_name=model_name,
+            classifier=model, test_idx=range(105, 150))
